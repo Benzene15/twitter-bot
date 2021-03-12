b36b99058ac6cfb301ee1df40a7e578ed460b4f7
Ben Cameron <camer343@umn.edu>
2021-03-12 00:10:41 -0600

    Fixing names when game is finished

--- app.js
+++ app.js
@@ -23,13 +23,7 @@ function makeDate(){
     date = d.getDate();
     return ['/'+year + '-' + month + '-' + date,date]; 
 }
-async function getTeamNamesAndInning(page, team1Spot, team2Spot){
-    const [el] = await page.$x("//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[1]/div/div[1]");
-    const txt = await el.getProperty('textContent');
-    var playing = await txt.jsonValue();
-    playing = playing.replace(/(\r\n|\n|\r)/gm,"");
-    playing = playing.replace(/\s/g, '');
-
+async function getTeamNames(page, team1Spot, team2Spot){
     const [en] = await page.$x(team1Spot);
     const x = await en.getProperty('textContent');
     var team1= await x.jsonValue();
@@ -42,7 +36,7 @@ async function getTeamNamesAndInning(page, team1Spot, team2Spot){
     team2=team2.replace(/(\r\n|\n|\r)/gm,"");
     team2=team2.replace(/\s/g, '');
 
-    return [playing,team1,team2]
+    return [team1,team2]
 }
 async function getScore(page){
     const [em]= await page.$x("//*[@id=\"tb-2-body row-0 col-0\"]/div");
@@ -77,122 +71,166 @@ async function scrapeScore(){
                 var playing;
                 var team1;
                 var team2;
-                [playing,team1,team2] = await getTeamNamesAndInning(page,
-                    "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[4]/div[1]/div[2]/div/a/div[2]/div[1]/div[1]",
-                    "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[4]/div[1]/div[3]/div/a/div[2]/div[1]/div[1]");
-                if(team1[0]!="Y" && team2[0]!="Y"){
-                    playingGame = false;
-                    console.log("Yankees are not playing today")
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
+                const [el] = await page.$x("//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[1]/div/div[1]");
+                const txt = await el.getProperty('textContent');
+                var playing = await txt.jsonValue();
+                playing = playing.replace(/(\r\n|\n|\r)/gm,"");
+                playing = playing.replace(/\s/g, '');
+                if(!playing.includes(":")){
+                    [team1,team2] = await getTeamNames(page,
+                        "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[2]/div/a/div[2]/div[1]/div[1]",
+                        "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[3]/div/a/div[2]/div[1]/div[1]");
+                        console.log(team1,team2)
+                    if(team1[0]!="Y" && team2[0]!="Y"){
+                        playingGame = false;
+                        console.log("Yankees are not playing today")
+                        while(d1.getDate()==searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
                     }
-                }
-                else if(playing.includes("Final")){
-                    [score1,score2] = await getScore(page);
-
-                    var yankeeScore;
-                    var otherScore;
-                    var otherName;
-                    if(team1[0]=="Y"){
-                        yankeeScore = parseInt(score1);
-                        otherScore = parseInt(score2);
-                        otherName = team2;
+                    else if(playing.includes("Final")){
+                        [score1,score2] = await getScore(page);
+    
+                        var yankeeScore;
+                        var otherScore;
+                        var otherName;
+                        if(team1[0]=="Y"){
+                            yankeeScore = parseInt(score1);
+                            otherScore = parseInt(score2);
+                            otherName = team2;
+                        }
+                        else{
+                            yankeeScore = parseInt(score2);
+                            otherScore = parseInt(score1);
+                            otherName = team1;
+                        }
+                        var score = "Final: Yankees: "+ yankeeScore + " " + otherName + ": "+ otherScore;
+                        console.log(score);
+                        if(yankeeScore<otherScore){
+                            var messageIndex=Math.floor(Math.random()*3);
+                            var messageArray=["Thank you to the " + otherName + " for sticking it to the Yankees today! ",
+                                    "What a beauitful day for a Yankees loss! ",
+                                    "Tough day to be a Yankees fan. Great day to be literally anyone else. "];
+                            var message = "DAAAAAAAAAAAAAAAAA JANKEES LOSE! ";
+                            var message1 = message.concat(messageArray[messageIndex]);
+                            var message2 = message1.concat(score);
+                            console.log(message2);
+                        }
+                        else{
+                            console.log("Yankees won :(")
+                        }
+                        playingGame = false;
+                        while(d1.getDate() == searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
+                    }
+                    else if(playing.includes("POSTPONED")){
+                        playingGame = false;
+                        console.log("Game is postponed")
+                        while(d1.getDate()==searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
                     }
                     else{
-                        yankeeScore = parseInt(score2);
-                        otherScore = parseInt(score1);
-                        otherName = team1;
-                    }
-                    var score = "Final: Yankees: "+ yankeeScore + " " + otherName + ": "+ otherScore;
-                    console.log(score);
-                    if(yankeeScore<otherScore){
-                        var messageIndex=Math.floor(Math.random()*3);
-                        var messageArray=["Thank you to the " + otherName + " for sticking it to the Yankees today! ",
-                                "What a beauitful day for a Yankees loss! ",
-                                "Tough day to be a Yankees fan. Great day to be literally anyone else. "];
-                        var message = "DAAAAAAAAAAAAAAAAA JANKEES LOSE! ";
-                        var message1 = message.concat(messageArray[messageIndex]);
-                        var message2 = message1.concat(score);
-                        console.log(message2);
-                    }
-                    else{
-                        console.log("Yankees won :(")
-                    }
-                    playingGame = false;
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
-                    }
-                }
-                else if(playing.includes("POSTPONED")){
-                    playingGame = false;
-                    console.log("Game is postponed")
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
+                        console.log(playing,team1,team2)
+                        await sleep(300000);
                     }
                 }
                 else{
-                    console.log(playing,team1,team2)
-                    await sleep(300000);
+                    console.log("Game at " + playing);
+                    await sleep(1800000);
+
                 }
+                
             }
             else{
                 await page.goto(full_url);
                 var playing;
                 var team1;
                 var team2;
-                [playing,team1,team2] = await getTeamNamesAndInning(page,
-                    "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[2]/div/a/div[2]/div[1]/div[1]",
-                    "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[3]/div/a/div[2]/div[1]/div[1]");
+                const [el] = await page.$x("//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[1]/div/div[1]");
+                const txt = await el.getProperty('textContent');
+                var playing = await txt.jsonValue();
+                playing = playing.replace(/(\r\n|\n|\r)/gm,"");
+                playing = playing.replace(/\s/g, '');
+                if(!playing.includes(":")){
+                    [team1,team2] = await getTeamNames(page,
+                        "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[2]/div/a/div[2]/div[1]/div[1]",
+                        "//*[@id=\"gridWrapper\"]/div/div[1]/div/div/div/div[2]/div[1]/div[3]/div[1]/div[3]/div/a/div[2]/div[1]/div[1]");
 
-                console.log(playing,team1,team2)
-                if(team1[0]!="Y" && team2[0]!="Y"){
-                    playingGame = false;
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
+                    console.log(playing,team1,team2)
+                    if(team1[0]!="Y" && team2[0]!="Y"){
+                        playingGame = false;
+                        while(d1.getDate()==searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
                     }
-                }
-                else if(playing.includes("Final")){
-                    [score1,score2] = await getScore(page);
+                    else if(playing.includes("Final")){
+                        [score1,score2] = await getScore(page);
 
-                    var yankeeScore;
-                    var otherScore;
-                    var otherName;
-                    if(team1[0]=="Y"){
-                        yankeeScore = parseInt(score1);
-                        otherScore = parseInt(score2);
-                        otherName = team2;
+                        var yankeeScore;
+                        var otherScore;
+                        var otherName;
+                        if(team1[0]=="Y"){
+                            yankeeScore = parseInt(score1);
+                            otherScore = parseInt(score2);
+                            otherName = team2;
+                        }
+                        else{
+                            yankeeScore = parseInt(score2);
+                            otherScore = parseInt(score1);
+                            otherName = team1;
+                        }
+                        var score = "Final: Yankees: "+ yankeeScore + " " + otherName + ": "+ otherScore;
+                        console.log(score);
+                        if(yankeeScore<otherScore){
+                            var messageIndex=Math.floor(Math.random()*3);
+                            var messageArray=["Thank you to the " + otherName + " for sticking it to the Yankees today! ",
+                                    "What a beauitful day for a Yankees loss! ",
+                                    "Tough day to be a Yankees fan. Great day to be literally anyone else. "];
+                            var message = "DAAAAAAAAAAAAAAAAA JANKEES LOSE! ";
+                            var message1 = message.concat(messageArray[messageIndex]);
+                            var message2 = message1.concat(score);
+                            console.log(message2);
+                        }
+                        playingGame = false;
+                        while(d1.getDate()==searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
+                    }
+                    else if(playing.includes("POSTPONED")){
+                        playingGame = false;
+                        while(d1.getDate()==searchDate){
+                            await sleep(1800000);
+                        }
+                        while(d1.getHours()<8){
+                            await sleep(1800000);
+                        }
                     }
                     else{
-                        yankeeScore = parseInt(score2);
-                        otherScore = parseInt(score1);
-                        otherName = team1;
-                    }
-                    var score = "Final: Yankees: "+ yankeeScore + " " + otherName + ": "+ otherScore;
-                    console.log(score);
-                    if(yankeeScore<otherScore){
-                        var messageIndex=Math.floor(Math.random()*3);
-                        var messageArray=["Thank you to the " + otherName + " for sticking it to the Yankees today! ",
-                                "What a beauitful day for a Yankees loss! ",
-                                "Tough day to be a Yankees fan. Great day to be literally anyone else. "];
-                        var message = "DAAAAAAAAAAAAAAAAA JANKEES LOSE! ";
-                        var message1 = message.concat(messageArray[messageIndex]);
-                        var message2 = message1.concat(score);
-                        console.log(message2);
-                    }
-                    playingGame = false;
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
-                    }
-                }
-                else if(playing.includes("POSTPONED")){
-                    playingGame = false;
-                    while(d1.getDate()==searchDate){
-                        await sleep(1800000);
+                        console.log("Waiting "+ playing)
+                        await sleep(300000);
                     }
                 }
                 else{
-                    console.log("Waiting "+ playing)
-                    await sleep(300000);
+                    console.log("Game at " + playing);
+                    await sleep(1800000);
+
                 }
             }
         }
